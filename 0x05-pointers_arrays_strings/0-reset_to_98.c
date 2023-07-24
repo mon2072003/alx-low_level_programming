f341cc12edb535956bd0d1d90174b361e6f50c03
mina <e.mena4248@gmail.com>
2023-07-24 06:50:49 -0700

    1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,31 +1,12 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-include <stdio.h>
-
-int _putchar (char c);
-
-void reset_to_98 (int *n);
-
-void swap_int (int *a, int *b);
-
-int _strlen(char *s);
-
-void _puts (char *str);
-
-void print_rev (char *s);
-
-void rev_string (char *s);
-
-void puts2 (char *str);
-
-void puts_half (char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy (char *dest, char *src);
-
-int _atoi(char *s);
-
-#endif /* MAIN-H */
+#include <unistd.h>
+/**
+* _putchar - writes the character c to stdout 
+* @1: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar (char c)
+{
+return (write (1, &c, 1));
+}
